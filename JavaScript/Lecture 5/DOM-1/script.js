8f43df88fd7e0eb2fafe64662ad2c08d15357643
Shivaji Singh <shivajisingh54@gmail.com>
2023-07-03 00:02:40 +0530

    f

---
+++ JavaScript/Lecture 5/DOM-1/script.js
@@ -0,0 +1,26 @@
+// DOM :- it's stand for Document Object Module.
+// BOM :- it's stand for Browser Object Module.
+
+// how to Access Elements ? :- lt's see in below methods.
+
+// getElementById('heading'), :- isse hum id ko get krte hain using this method.
+// it's return a single object.
+
+// getElementsByClassName('write class name here') :- isse hum class ko get krte hain using this method.
+// it's return a multiple object (array like).
+
+// getElementsByTagName('write TagName here') :- isse hum class ko get krte hain using this method.
+// it's return a multiple object (array like).
+
+// getElementsByClassName() and getElementsByTagName() :-
+// 1. both methods are use document object.
+// 2. both methods are return multiple items.
+// 3. the list returned is not an Array like.
+
+// querySelector :- is method se bhi hum id,class and tag ko access kr skte h but ek baar me ek hi items ko access krega and agar id h to uske liye # is symple ka use krte pehle phir id ka naam likhate hain same for class.
+// iska hamesh single object output hota hain.
+// example :-
+// let a = querySelector('#header') :- it's id
+// let b = querySelector('.header') :- it's class
+// let c = querySelector('header')  :- it's tag
+
